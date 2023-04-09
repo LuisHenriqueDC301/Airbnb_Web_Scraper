500847d59c767101de1992e6c87d7361007a3558
LuisHenriqueDC301 <113612000+LuisHenriqueDC301@users.noreply.github.com>
2023-04-09 15:59:16 -0300

    First Commit

---
+++ AirbnbScreper.py
@@ -0,0 +1,109 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+nome_cidade = input("Cidade: ")  # solicita ao usuário que informe o nome da cidade
+data_ida = input("Digite a data de ida (ano-mês-dia):")
+data_volta = input("Digite a data de volta (ano-mês-dia):")
+
+options= Options()  # instancia um objeto da classe Options
+options.add_argument("window-size=950,800")  # define o tamanho da janela do navegador
+#options.add_argument("--headless")  # opção para executar o navegador em modo headless
+
+#Tive que colocar o caminho especifico do meu chrome web driver pois estava dando erro, (não é obrigatorio)
+pat = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Python 3.11\chromedriver"  # define o caminho do webdriver do Chrome
+navegador = webdriver.Chrome(executable_path=pat, options=options)  # instancia um objeto da classe webdriver.Chrome passando o caminho do webdriver e as opções definidas anteriormente
+
+navegador.get("http://airbnb.com")  # navega para o site do Airbnb
+sleep(2)  # espera por 2 segundos
+
+Click = navegador.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/div/div/div/header/div/div[2]/div[1]/div/button[3]/div[2]').click()  # encontra o botão "Aceitar cookies" e clica nele
+
+sleep(1.5)  # espera por 3 segundos
+
+pesquisar = navegador.find_element(By.XPATH, '//*[@id="bigsearch-query-location-input"]').click()  # encontra o campo de pesquisa e clica nele
+procurar_cidade = navegador.find_element(By.NAME, "query")  # encontra o campo de pesquisa por nome
+procurar_cidade.send_keys(nome_cidade)  # insere o nome da cidade no campo de pesquisa
+procurar_cidade.submit()  # envia a pesquisa
+
+
+sleep(4)
+url_atual = navegador.current_url
+nova_url = f"{url_atual}&date_picker_type=calendar&checkin={data_ida}&checkout={data_volta}"
+
+navegador.get(nova_url)
+sleep(10)
+
+conteudo_pagina = navegador.page_source
+site = BeautifulSoup(conteudo_pagina, "html.parser")
+
+# encontra todos os elementos div com o atributo itemprop="itemListElement"
+lista_hospedagem = site.find_all('div', attrs={'itemprop': "itemListElement"})
+# obtém o primeiro elemento div da lista de hospedagem
+
+#Criar meu dicionario para colocar as informações:
+dic_dados = {
+"Url": [],
+"Informação": [],
+"Avaliação" : [],
+"Local": [],
+"Preço" : []
+}
+
+for hospedagem in lista_hospedagem:
+    try:
+        # obtém a URL da hospedagem
+        hospedagem_url = hospedagem.find('meta', attrs={'itemprop': "url"})['content']
+        # navega para a página da hospedagem
+        navegador.get(f"http://{hospedagem_url}")
+    
+        # espera por 10 segundos
+        sleep(10)
+
+        # obtém o conteúdo da página atual do navegador e converte para um objeto BeautifulSoup
+        conteudo_pagina = navegador.page_source
+        site = BeautifulSoup(navegador.page_source, "html.parser")
+
+
+        # encontra os dados
+        hospedagem_infor =  site.find_all('h1', attrs={"elementtiming":"LCP-target"})[0].text
+        
+        hospedagem_local = site.find_all('div', attrs={'data-plugin-in-point-id':"TITLE_DEFAULT"})[0].find_all('button')[1].text
+
+        hospedagem_preco = site.find('div',attrs={'data-testid':"book-it-default"}).find_all("span")[2].text
+        
+        if hospedagem_preco[0] == "R":
+            hospedagem_preco = hospedagem_preco[2:]
+        else:
+            hospedagem_preco = site.find('div',attrs={'data-testid':"book-it-default"}).find_all("span")[1].text
+            hospedagem_preco = hospedagem_preco[2:]
+            
+  
+        hospedagem_avaliacao = site.find_all('span', attrs={'aria-hidden':"true"})[0].text
+        hospedagem_avaliacao = float(hospedagem_avaliacao.split(" ")[0].replace(',', "."))
+
+        dic_dados["Url"].append(hospedagem_url)
+        dic_dados["Avaliação"].append(hospedagem_avaliacao)
+        dic_dados["Informação"].append(hospedagem_infor)
+        dic_dados["Local"].append(hospedagem_local)
+        dic_dados['Preço'].append(hospedagem_preco)
+        
+    except ValueError:
+        hospedagem_avaliacao = None
+        dic_dados["Url"].append(hospedagem_url)
+        dic_dados["Avaliação"].append(hospedagem_avaliacao)
+        dic_dados["Informação"].append(hospedagem_infor)
+        dic_dados["Local"].append(hospedagem_local)
+        dic_dados['Preço'].append(hospedagem_preco)
+        
+    except Exception as e:
+        input(f"Algo deu errado, {e} //| {type(e).__name__}")
+     
+        
+df = pd.DataFrame(dic_dados)
+df.to_csv('dados.csv', index=False)
+
