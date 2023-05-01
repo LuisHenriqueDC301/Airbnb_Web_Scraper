c0ca02484ed29555c55b0a3b5ce128b87c50eb83
LuisHenriqueDC301 <113612000+LuisHenriqueDC301@users.noreply.github.com>
2023-05-01 19:39:47 -0300

    Adicionei Reviews

--- AirbnbScraper.py
+++ AirbnbScraper.py
@@ -1,5 +1,6 @@
 # Importando as bibliotecas necessárias
 import pandas as pd
+from lxml import etree
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -69,6 +70,15 @@ dic_dados = {
     "Preco" : []
 }
 
+dic_reviews = {
+    "Limpeza:": [],
+    "Exatidao do Anuncio" : [],
+    "Comunicacao": [],
+    "Localizacao": [],
+    "Check-In": [],
+    "Custo Beneficio" : []
+}
+
 # Loop pelos elementos da lista de hospedagem
 for hospedagem in lista_hospedagem:
     try:
@@ -79,23 +89,25 @@ for hospedagem in lista_hospedagem:
         navegador.get(f"http://{hospedagem_url}")
     
         # Aguarda 10 segundos para a página ser carregada completamente
-        sleep(10)
+        sleep(15)
 
         # Obtém o conteúdo da página atual do navegador e converte para um objeto BeautifulSoup
         conteudo_pagina = navegador.page_source
         site = BeautifulSoup(navegador.page_source, "html.parser")
 
         # Encontra as informações da hospedagem na página
-        hospedagem_infor =  site.find_all('h1', attrs={"elementtiming":"LCP"} )
+        hospedagem_infor =  site.find_all('h1', attrs={"elementtiming":"LCP-target"})[0].text
         
         # Encontra a avaliação da hospedagem na página
-        hospedagem_avaliacao = site.find_all('span', attrs={"class":"_1ne5r4rt"})[0].text
+        hospedagem_avaliacao = site.find_all('span', attrs={'aria-hidden':"true"})[0].text
+        hospedagem_avaliacao = float(hospedagem_avaliacao.split(" ")[0].replace(',', "."))
+
         
         # Encontra a localização da hospedagem na página
-        hospedagem_local = site.find_all('span', attrs={"class":"_16shi2n"})[0].text
+        hospedagem_local = site.find_all('div', attrs={'data-plugin-in-point-id':"TITLE_DEFAULT"})[0].find_all('button')[1].text
         
         # Encontra o preço da hospedagem na página
-        hospedagem_preco = site.find_all('span', attrs={'class': '_olc9rf0'})[0].text
+        hospedagem_preco = site.find('div',attrs={'data-testid':"book-it-default"}).find_all("span")[2].text
         
         # Adiciona as informações ao dicionário de dados
         dic_dados["Url"].append(hospedagem_url)
@@ -103,13 +115,66 @@ for hospedagem in lista_hospedagem:
         dic_dados["Avaliacao"].append(hospedagem_avaliacao)
         dic_dados["Local"].append(hospedagem_local)
         dic_dados["Preco"].append(hospedagem_preco)
+
+        #Entra os dados de reviews:
+        dom = etree.HTML(str(site))
+
+        review_limpeza = dom.xpath('//*[@id="site-content"]/div/div[1]/div[4]/div/div/div/div[2]/section/div[2]/div/div/div[1]/div/div/div[2]/span')[0].text
+        review_anuncio = dom.xpath('//*[@id="site-content"]/div/div[1]/div[4]/div/div/div/div[2]/section/div[2]/div/div/div[2]/div/div/div[2]/span')[0].text
+        review_comunicacao = dom.xpath('//*[@id="site-content"]/div/div[1]/div[4]/div/div/div/div[2]/section/div[2]/div/div/div[3]/div/div/div[2]/span')[0].text
+        review_localizacao = dom.xpath('//*[@id="site-content"]/div/div[1]/div[4]/div/div/div/div[2]/section/div[2]/div/div/div[4]/div/div/div[2]/span')[0].text
+        review_check_in = dom.xpath('//*[@id="site-content"]/div/div[1]/div[4]/div/div/div/div[2]/section/div[2]/div/div/div[5]/div/div/div[2]/span')[0].text
+        review_custo_beneficio = dom.xpath('//*[@id="site-content"]/div/div[1]/div[4]/div/div/div/div[2]/section/div[2]/div/div/div[6]/div/div/div[2]/span')[0].text
+
+        # Adiciona as informações ao dicionário de reviews
+        dic_reviews['Limpeza:'].append(review_limpeza)
+        dic_reviews['Exatidao do Anuncio'].append(review_anuncio)
+        dic_reviews["Comunicacao"].append(review_comunicacao)
+        dic_reviews["Localizacao"].append(review_localizacao)
+        dic_reviews["Check-In"].append(review_check_in)
+        dic_reviews["Custo Beneficio"].append(review_custo_beneficio)
+
+        if hospedagem_preco[0] == "R":
+         hospedagem_preco = hospedagem_preco[2:]
+        else:
+            hospedagem_preco = site.find('div',attrs={'data-testid':"book-it-default"}).find_all("span")[1].text
+            hospedagem_preco = hospedagem_preco[2:]
         
-    except:
-        continue
+
+        hospedagem_avaliacao = site.find_all('span', attrs={'aria-hidden':"true"})[0].text
+        hospedagem_avaliacao = float(hospedagem_avaliacao.split(" ")[0].replace(',', "."))
+        
+        print(f"A url: {hospedagem_url}")
+
+
+    # Caso o quarto não possuir uma avaliação, vai dar um erro de valor e a hospedagem_avalição recebera nulo
+    except ValueError:
+     hospedagem_avaliacao = None 
+     # Adiciona as informações ao dicionário de dados
+     dic_dados["Url"].append(hospedagem_url)
+     dic_dados["Informacao"].append(hospedagem_infor)
+     dic_dados["Avaliacao"].append(hospedagem_avaliacao)
+     dic_dados["Local"].append(hospedagem_local)
+     dic_dados["Preco"].append(hospedagem_preco)
+        
+     # Adiciona as informações ao dicionário de reviews
+     dic_reviews['Limpeza:'].append(review_limpeza)
+     dic_reviews['Exatidao do Anuncio'].append(review_anuncio)
+     dic_reviews["Comunicacao"].append(review_comunicacao)
+     dic_reviews["Localizacao"].append(review_localizacao)
+     dic_reviews["Check-In"].append(review_check_in)
+     dic_reviews["Custo Beneficio"].append(review_custo_beneficio)
+
+     continue
+
 
 #Usa a blibioteca Pandas para salvar os dados com um dataframe        
-df = pd.DataFrame(dic_dados)
+df_listing = pd.DataFrame(dic_dados)
+df_reviews = pd.DataFrame(dic_reviews)
 
 #Salva o dataframe em um arquivo csv
-df.to_csv('dados.csv', index=False)
+df_listing.to_csv(f'{nome_cidade}_listing.csv', index=True)
+df_reviews.to_csv(f"{nome_cidade}_reviews.csv", index=True)
 
+print(df_listing, df_reviews)
+input()
